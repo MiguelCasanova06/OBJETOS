4fae7c046e244dc8bdb724102e5c1769a828d3df
Miguel <miguel1234.mca@gmail.com>
2025-05-13 12:48:38 +0200

    Realizada la documentación del método existe

--- src/es/mdc/FUtil.java
+++ src/es/mdc/FUtil.java
@@ -2,7 +2,11 @@ package es.mdc;
 import java.io.File;
 
 public class FUtil {
-
+/**
+ * Comprueba si un fichero existe o no
+ * @param fichero a comprobar
+ * @return true si el fichero existe, false si no existe
+ */
     public static boolean existe(String filename) {
         if (filename == null || filename.trim().isEmpty()) {
             throw new IllegalArgumentException("El nombre del archivo está vacío o contiene solo espacios.");
